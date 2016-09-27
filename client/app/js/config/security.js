6134fece711cd6854378bd98e4dc6dfcd38f3876
Attila Kling <attila.kling@timelinerx.com>
2016-09-27 22:49:36 +0200

    Added angular2 client

--- client/app/js/config/security.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-export const REDIRECT_IF_NOT_AUTHENTICATED = 'redirectifnotauthenticated';
-export const SKIP_IF_AUTHENTICATED = 'skipifauthenticated';
--- client/app/js/user/user-service.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-export default class UserService {
-
-  static get $inject() {
-    return [];
-  }
-
-}
